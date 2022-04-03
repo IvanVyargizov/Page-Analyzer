cb158df191b1da0bef812f89c954a72fa83f59a4
Ivan_Vyargizov <vyargizov@gmail.com>
2022-04-03 11:18:39 +0400

    init file

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,1 +1,20 @@
+package hexlet.code;
 
+import io.javalin.Javalin;
+
+public class App {
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> config.enableDevLogging());
+        app.before(ctx -> ctx.attribute("ctx", ctx));
+        return app;
+    }
+
+    public static void main(String[] args) {
+        final int port = 5050;
+        Javalin app = getApp();
+        app.start(port);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+
+}
