9523ae6e9f735259ec50c16f603550299c6b13f9
Ivan_Vyargizov <vyargizov@gmail.com>
2022-04-10 23:02:35 +0400

    add to ignore .db

---
+++ src/main/java/hexlet/code/domain/Url.java
@@ -0,0 +1,49 @@
+package hexlet.code;
+
+import io.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import io.ebean.annotation.WhenCreated;
+import java.time.Instant;
+
+@Entity
+public final class Url extends Model {
+
+    @Id
+    private long id;
+
+    @WhenCreated
+    private Instant createdAt;
+
+    private String name;
+
+    @OneToMany
+    private List<UrlCheck> urlChecks;
+
+    public Url() {
+    }
+
+    public Url(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Instant getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public List<UrlCheck> getUrlChecks() {
+        return urlChecks;
+    }
+
+}
